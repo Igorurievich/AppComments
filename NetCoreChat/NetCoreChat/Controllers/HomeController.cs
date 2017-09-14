5564d65e36296f84c8ccdca721ec50ae89ec3b53
SIIPOLSKA\ibodia <ibodia@pl.sii.eu>
2017-09-14 20:04:24 +0200

    Update apllication to version of .NET Core 2.0. Add some logic.

--- NetCoreChat/NetCoreChat/Controllers/HomeController.cs
+++ NetCoreChat/NetCoreChat/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NetCoreChat.Models;
 
 namespace NetCoreChat.Controllers
 {
@@ -29,7 +31,7 @@ namespace NetCoreChat.Controllers
 
         public IActionResult Error()
         {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
