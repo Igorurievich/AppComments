a9545ae526469ae6cb3cf47dafac984f6e1b2ec8
SIIPOLSKA\ibodia <ibodia@pl.sii.eu>
2017-09-14 17:16:56 +0200

    Initial commit

---
+++ NetCoreChat/NetCoreChat/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreChat.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
