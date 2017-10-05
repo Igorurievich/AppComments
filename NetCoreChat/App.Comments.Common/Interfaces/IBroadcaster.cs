83b9897d0d071711a791ec7172b680a01fa63cde
SIIPOLSKA\ibodia <ibodia@pl.sii.eu>
2017-10-05 12:21:12 +0200

    Add IBrodcaster interface.

---
+++ NetCoreChat/App.Comments.Common/Interfaces/IBroadcaster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace App.Comments.Common.Interfaces {
+
+    public interface IBroadcaster
+    {
+        Task SetConnectionId(string connectionId);
+        //Task UpdateMatch(MatchViewModel match);
+        //Task AddFeed(FeedViewModel feed);
+        //Task AddChatMessage(ChatMessage message);
+    }
+}
--- NetCoreChat/NetCoreChat/Hubs/Broadcaster.cs
+++ NetCoreChat/NetCoreChat/Hubs/Broadcaster.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using App.Comments.Common.Interfaces;
+using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace App.Comments.Web.Hubs
 {
-    public class Broadcaster
+    public class Broadcaster : Hub<IBroadcaster>
     {
+        public override Task OnConnected()
+        {
+            // Set connection id for just connected client only
+            return Clients.Client(Context.ConnectionId).SetConnectionId(Context.ConnectionId);
+        }
+
+        // Server side methods called from client
+        public Task Subscribe(int matchId)
+        {
+            return Groups.Add(Context.ConnectionId, matchId.ToString());
+        }
+
+        public Task Unsubscribe(int matchId)
+        {
+            return Groups.Remove(Context.ConnectionId, matchId.ToString());
+        }
     }
 }
